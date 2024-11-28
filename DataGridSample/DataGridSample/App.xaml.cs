469a7514c5cb458930e4a710f5aca512deb53d1c
SethupathyD <sethupathy.d@syncfusion.com>
2024-11-28 16:24:27 +0530

    Code updated

--- DataGridSample/DataGridSample/App.xaml.cs
+++ DataGridSample/DataGridSample/App.xaml.cs
@@ -5,7 +5,6 @@
         public App()
         {
             InitializeComponent();
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1FpRHxbf1x0ZFdMZV5bQX9PMyBoS35RckRiWXpeeHRWRmZaU0R+");
 
             MainPage = new AppShell();
         }

90037ca9b825e31d1d3fbd5a26569a609c4b7787
SethupathyD <sethupathy.d@syncfusion.com>
2024-11-28 15:44:49 +0530

    Code updated regarding the export DataGridImageColumn to pdf.

---
+++ DataGridSample/DataGridSample/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DataGridSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1FpRHxbf1x0ZFdMZV5bQX9PMyBoS35RckRiWXpeeHRWRmZaU0R+");
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DataGridSample/DataGridSample/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.Maui.DataGrid.Exporting;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+
+namespace DataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            MemoryStream stream = new MemoryStream();
+            DataGridPdfExportingController pdfExport = new DataGridPdfExportingController();
+            DataGridPdfExportingOption option = new DataGridPdfExportingOption();
+            option.CanExportColumnWidth = true;
+            var pdfDoc = new PdfDocument();
+            pdfDoc = pdfExport.ExportToPdf(this.dataGrid, option);
+            pdfDoc.Save(stream);
+            pdfDoc.Close(true);
+            SaveService saveService = new();
+            saveService.SaveAndView("ExportFeature.pdf", "application/pdf", stream); ;
+        }
+    }
+}
